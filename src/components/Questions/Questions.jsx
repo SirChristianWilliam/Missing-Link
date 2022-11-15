20d2695a4d8e1c53b57108d1b9be7ede30474fa5
Chris Maki <c.william.developer@gmail.com>
2022-11-15 14:22:47 -0600

    questions page entities set up complete

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -7,11 +7,38 @@
 // THERE WILL PROBABLY BE NO NEED FOR A SUBMIT BUTTON AT THE END
 // THEN, I'LL JUST STATE 'YOUR DATA IS AUTOMATICALLY SAVED.' 
 // INCLUDE THE NOTE TO ENSURE ANONYMITY.
- 
+function handleChange(event){
+    event.preventDefault();
+    console.log('handleChange, event.target.value is', event.target.value);
+};
 function Questions() {
     return(
         <>
             <h1>Questions Page</h1>
+            <p>Please note: In order to receive honest responses,
+                answers are uploaded anonymously. No one will be
+                able to see who is associated with these results, as
+                they are purely for research purposes.
+            </p>
+            <p>Please type your answer in the given format shown.</p>
+            <table className="questionsTableContainer">
+                <thead>
+                    <tr>
+                        <th>Questions:</th><th>Answers</th>
+                    </tr>  
+                    <tr>
+                        <td>Ever heard of updog?</td>
+                        <td>
+                            <input 
+                                type='text'
+                                // placeholder={questions.placeholder_hint}
+                                value='remember put placeholder here'
+                                onChange={handleChange}
+                            />
+                        </td>
+                    </tr>
+                </thead>
+            </table>
         </>
     )
 };
