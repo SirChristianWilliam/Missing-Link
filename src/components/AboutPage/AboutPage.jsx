ace26230fe570e200e1828e3e5bd49240e9362aa
Chris Maki <c.william.developer@gmail.com>
2022-11-15 14:25:14 -0600

    about page component entities set up

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,27 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>
+          Thank you Prime Digital Academy for teaching me 
+          the tools needed to create this project. Specifically, 
+          thank you Edan- your methods of teaching are perfect and 
+          you always answer any questions I have. Thanks to my 
+          cohort Ramirez, who are always willing to share their 
+          knowledge- I’ve learned so much from you all! Thank you 
+          to those in my life who I’m close to who understand my 
+          need to be selfish with my time while I’m attending this 
+          program. A big thank you to anyone who’s helped me 
+          even though I may not have realized it at the time, 
+          you’re amazing. 
+        </p>
+        <p>
+          Technologies used: 
+          Javascript, React, 
+          Redux, Postico, 
+          postql, Postman, 
+          Figma, Vscode, 
+          HTML, CSS, AWS. 
+        </p>
       </div>
     </div>
   );
