5eb7e060acb62851c5a9dc6a4dff785aad666def
Chris Maki <c.william.developer@gmail.com>
2022-11-15 14:10:54 -0600

    profile page has all entities it needs, primed and ready for logic

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -13,13 +13,102 @@
 // TO UPLOAD(YOU SAW THIS IN CLASS FROM STUDENT PRESENTATION),
 // OR JUST A BIG LETTER, GRABBED FROM THE FIRST LETTER
 // OF THE USER'S NAME.
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
+function changePic() {
+    console.log('Change pic clicked');
+    //* onClick, open images folder to replace 
+    //this image to one of your choosing.
+
+}
 function Profile() {
-    return(
+    const history = useHistory();
+    const user = useSelector((store) => store.user);
+
+function editUsername(event) {
+    event.preventDefault();
+    console.log('edit username button clicked',event.target.value)
+};
+
+function editEmail(event)  {
+    event.preventDefault();
+    console.log('edit email button clicked',event.target.value)
+};
+function deleteRow(event) {
+    event.preventDefault();
+    console.log('Profile delete clicked',event.target.value)
+}
+    return (
         <>
-            
-            <h1>Profile Page(maybe user page??)</h1>
-        </>
+            <h1>Profile Page</h1> 
+{/* LEFT SIDE OF PROFILE PAGE */}
+            <div className='profileLeftContainer'>
+                <img
+                    onClick={changePic}
+                    className="ppic"
+                    src="ppic.png">
+                </img>
+                <div>
+                    <h3>Username:</h3>
+                    <span>
+                        {user.username}
+                        <button
+                            onClick={editUsername}
+                        >
+                        Edit
+                        </button>
+                    </span>
+                </div>
+
+                <div>
+                    <h3>Email:</h3>
+                    <span>
+                        {user.email}
+                        <button
+                            onClick={editEmail}
+                        >Edit
+                        </button>
+                    </span>
+                </div>
+
+                <div>
+                    <p>
+                    ⬇ Click button to fill out your own personal questionnaire.
+                    </p>
+                    <button
+                        onClick={()=>{history.push('/questions')}}
+                    >
+                    Let's answer some questions!
+                    </button>
+                </div>
+            </div>
+{/* END */}
+{/* RIGHT SIDE OF PROFILE PAGE */}
+
+    <div className='profileRightContainer'>
+        <h2>Your personal condtions list:</h2>
+        <table className='profileTableContainer'>
+            <tr>
+                <th>Condition</th><th>Key Access?</th><th>Delete</th>
+            </tr>
+            <tr>
+                <td>- map saved condition -</td>
+                <td>- map boolean value for that condition, show checkmark -</td>
+                <td><button
+                    onClick={deleteRow}
+                >Delete
+                </button>
+                </td>
+            </tr>
+
+        </table>
+
+    </div>
+
+{/* END */}
+    </>
 
     )
 };
