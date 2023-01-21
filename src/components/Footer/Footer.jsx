3b814658b83c8f8786cd9a093898b3cb3a1e4e56
Chris Maki <c.william.developer@gmail.com>
2023-01-20 18:17:50 -0600

    Changed footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <footer>&copy; Chris™️ Digital Academy ® </footer>;
+  return <footer>&copy; Missing Link, by Chris Maki </footer>;
 };
 
 export default Footer;
