a5cf5371f1b0c49734040b7c588b54d7fa28c5d6
Chris Maki <c.william.developer@gmail.com>
2023-01-27 20:45:32 -0600

    Modified store.js from createStore to legacy_createStore. Not much of a change but I need to make sure it still works when deployed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
+
 
 import rootReducer from './reducers/_root.reducer'; // imports ./redux/reducers/index.js
 import rootSaga from './sagas/_root.saga'; // imports ./redux/sagas/index.js
