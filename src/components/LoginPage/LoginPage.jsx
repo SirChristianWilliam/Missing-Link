6458e473b27aebc0495ff526d22a7cb151965cbf
Chris Maki <100000473+SirChristianWilliam@users.noreply.github.com>
2023-07-11 08:36:46 -0500

    Update LoginPage.jsx
    Explanation for log in and register

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -19,6 +19,12 @@ function LoginPage() {
             </button>
         </center>
     </div>
+      <h3> YOU WILL NOT BE BE ABLE TO LOG IN OR REGISTER UNTIL FURTHER NOTICE. (BIT.IO) IS NO LONGER IN SERVICE, RESULTING IN A MISSING DATABASE. TO VIEW 
+          HOW THE APPLICATION WORKS, PLEASE WATCH THE VIDEO DEMONSTRATION HERE: 
+          <a href="https://www.youtube.com/watch?v=KXYbTYLZoBw&list=PLo9U1DCI2cVyDJR5vJefvzTc7bvt02wK9&index=1">
+            Missing Link Demo Video
+          </a>
+     </h3>
   );
 };
 
