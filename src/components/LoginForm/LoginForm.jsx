da74045256a32662a96fbbe4cc3263c1f8fbb180
Chris Maki <100000473+SirChristianWilliam@users.noreply.github.com>
2023-07-11 08:33:56 -0500

    Update LoginForm.jsx
    Explanation added for missing database

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -97,6 +97,13 @@ function LoginForm() {
                 </Button>
       
     </form>
+    <h3> YOU WILL NOT BE BE ABLE TO LOG IN UNTIL FURTHER NOTICE. (BIT.IO) IS NO LONGER IN SERVICE, RESULTING IN A MISSING DATABASE. TO VIEW 
+          HOW THE APPLICATION WORKS, PLEASE WATCH THE VIDEO DEMONSTRATION HERE: 
+          <a href="https://www.youtube.com/watch?v=KXYbTYLZoBw&list=PLo9U1DCI2cVyDJR5vJefvzTc7bvt02wK9&index=1">
+            Missing Link Demo Video
+          </a>
+     </h3>
+            
   );
 };
 
